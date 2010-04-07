1fa03a85a48ee6ea8eb62277bf25d8548237554b
nateperry333@gmail.com <nateperry333@gmail.com@a5e5e314-9056-e45e-a1cb-51fd10de16d5>
2010-04-07 02:14:56 +0000

    Initial incomplete script.

---
+++ sort-photos.pl
@@ -0,0 +1,512 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+sort-photos - Sort photos from one directory into another.
+
+=head1 SYNOPSIS
+
+Sorts all the photos from one directory into another directory based on the
+date the photo was taken.
+
+=cut
+
+
+use warnings;
+use strict;
+
+use File::Path;
+use File::Spec;
+use Getopt::Long;
+use Image::Magick;
+use Pod::Usage;
+use POSIX ();
+use Time::localtime;
+use Term::ReadKey;
+
+
+my ($VOLUME, $DIRECTORIES, $SCRIPT) = File::Spec->splitpath($0);
+
+my $source = "./tosort"; # source folder
+my $dest   = "./sorted"; # destination folder
+
+# get directory listing of source
+
+# iterate over each file
+    # is picture
+	# get year, month and day photo was taken
+	# create directory as $dest/$year/$month/$day
+	# drop file in folder just created
+
+=head1 USAGE
+
+  sort-images.pl -h
+
+  sort-images.pl -v
+
+  sort-images.pl <options> -r <directory> <directory> ... <target-directory>
+
+=head2 OPTIONS
+
+=over 8
+
+=item -h --help
+
+Print a help message and exits.
+
+=item --version
+
+Print a breif message and exits.
+
+=cut
+
+#=item -s --sort
+#
+#Specifies how to sort the images.  This may be specified multiple times for
+#sub-sorting.
+#
+#  sort option
+#  ------------------------------
+#  --sort image <image-property>
+#  --sort file <file-property>
+
+=item -q --quiet
+
+Show less output.
+
+=item -v --verbose
+
+Show more output.
+
+=item -r -R --recursive
+
+Operate recursively (down directory tree).
+
+=back
+
+=cut
+
+
+my $DEBUG = 1;
+my $WARNINGS = 2;
+
+# Verbosity of output.
+#  0  Minimal
+#  1  Normal
+#  2  Verbose
+my $verbosity = 2;
+
+my $recursive = 0;
+
+=head1 DESCRIPTION
+
+Generates files needed to compile the dictionary and images into the Duet
+project as well as binary files used to write various different tables.
+
+=cut
+
+
+ProcessFolder($source);
+
+
+sub ParseOptions
+{
+    #my ($option_log_file, $option_show_help, $option_image_compression, $option_all, $option_variant) = undef;
+    #my ($option_std_verboseness, $option_log_verboseness, $option_fake) = 0;
+    #
+    #GetOptions
+    #(
+    #    'help|h'        => \$option_show_help,
+    #    'verbose|v+'    => \$option_std_verboseness,
+    #    'log|l+'        => \$option_log_verboseness,
+    #    'logfile:s'     => \$option_log_file,
+    #    'fake|f'        => \$option_fake,
+    #    'all|a'         => \$option_all,
+    #    'compression:s' => \$option_image_compression,
+    #    'variant:s'     => \$option_variant,
+    #);
+    #
+    #if ($option_show_help)
+    #{
+    #    pod2usage
+    #    (
+    #      -exitval => 0,
+    #      -verbose => 99,
+    #      -sections => "NAME|SYNOPSIS|USAGE|USAGE/OPTIONS"
+    #    ); # this will exit the script here.
+    #
+    #    exit 0; # should not be reached.
+    #}
+    #
+    #if (defined($option_std_verboseness))
+    #{
+    #    $Std = $option_std_verboseness;
+    #    if ($Std)
+    #    {
+    #        print("Standard verboseness set to " . $Std);
+    #        print(" (" . $Std_Verboseness{$Std} . ")")
+    #            if (exists $Std_Verboseness{$Std});
+    #        print(".\n");
+    #    }
+    #}
+    #
+    #if (defined($option_log_verboseness))
+    #{
+    #    $Log = $option_log_verboseness;
+    #    if ($Std)
+    #    {
+    #        print("Logging verboseness set to " . $Log);
+    #        print(" (" . $Log_Verboseness{$Log} . ")")
+    #            if (exists $Log_Verboseness{$Log});
+    #        print(".\n");
+    #    }
+    #
+    #}
+    #
+    #if ($option_log_file)
+    #{
+    #    $Files{log} = File::Spec->rel2abs($option_log_file);
+    #    Logging::LogFile($Files{log});
+    #    if (!$Log)
+    #    {
+    #        $Log = 1;
+    #    }
+    #}
+    #else
+    #{
+    #    $Files{log} = $Default_Log_File;
+    #}
+    #
+    #if ($Log)
+    #{
+    #    print("logging set to '" . Logging::LogFile() . "'.\n")
+    #        if (1 <= $Std);
+    #    Logging::RawWriteLine();
+    #    Logging::WriteLine("----- Logging started -----");
+    #}
+    #else
+    #{
+    #    print("Logging disabled.\n")
+    #      if ($Std);
+    #}
+    #
+    #if ($option_image_compression)
+    #{
+    #    foreach my $type (keys(%Image_Compression_Types))
+    #    {
+    #        if (lc($type) eq lc($option_image_compression))
+    #        {
+    #            # $Image_Compression_Types{}
+    #            # $Image_Compression_Names{}
+    #            $Image_Compression = $Image_Compression_Types{$type};
+    #            print("Image compression set to " . $Image_Compression_Names{$Image_Compression_Types{$type}} . "\n") if ($Std);
+    #        }
+    #    }
+    #}
+    #
+    #if (defined($option_fake))
+    #{
+    #    $RealMode = !$option_fake;
+    #    print("'Fake' file writing mode enabled.\n")
+    #      if ($Std);
+    #}
+    #
+    #if (defined($option_all))
+    #{
+    #    $AllMode = $option_all;
+    #    print("'All' mode enabled.  Will generate all variants of the static talbe data file.\n")
+    #      if ($Std);
+    #}
+    #
+    #if (defined($option_variant))
+    #{
+    #    $Variant = $option_variant;
+    #}
+    #
+    #print("\n") if ($Std);
+}
+
+sub process_picture {
+	# $File::Find::dir	is the current directory name
+	# $_				is the current filename within that directory
+	# #File::Fine::name is the complete pathname to the file
+
+	print "processing " . $_;
+	
+	#!/usr/local/bin/perl
+	use Image::Magick;
+
+	my $image = Image::Magick->new;
+	my $x = $image->Read($_);
+	warn "$x" if "$x";
+
+	
+	print "\n";
+
+}
+
+
+sub ProcessFolder
+{
+    my $path = shift;
+
+    my @files = ListFiles($path);
+    my $count = @files;
+
+    if (!$count)
+    {
+        return;
+    }
+
+    ClearConsoleLine() if ($verbosity);
+    print("$path : $count") if ($verbosity);
+    print("\n") if (1 < $verbosity);
+
+    my $image_magick = Image::Magick->new;
+
+    #for (my $x = 0; $image_magick->[$x]; $x++)
+    foreach my $file (@files)
+    {
+        my $base_filename = $path . '/' . $file;
+
+        ClearConsoleLine() if ($verbosity);
+        print("$file") if ($verbosity);
+
+        if (-d $base_filename)
+        {
+            # Is a directory, process it now.
+            # exit function here.
+            #ProcessFolder($base_filename) if $recursive;
+            next;
+        }
+
+        my $image_count_or_error = $image_magick->Read($base_filename);
+        my $image_count;
+        if ("$image_count_or_error")
+        {
+            $image_count = 0;
+            print STDERR "WARNING: Failed to parse a directory: $path, $image_count_or_error\n" if ($WARNINGS);
+            next;
+        }
+        else
+        {
+            $image_count = 0+$image_count_or_error;
+            #print STDERR "NOTICE: $image_count $path image(s) found.\n" if ($DEBUG);
+        }
+
+        #my $base_filename = $image_magick->[$x]->Get('base-filename');
+        my $width  = $image_magick->Get('width');
+        my $height = $image_magick->Get('height');
+        my $usertime = $image_magick->Get('user-time');
+
+        $base_filename = File::Spec->rel2abs($base_filename);
+        my ($file_volume, $file_directories, $file_name) = File::Spec->splitpath($base_filename);
+
+        ClearConsoleLine() if ($verbosity);
+        print("$file_name $width by $height, $usertime") if ($verbosity);
+        print("\n") if (1 < $verbosity);
+
+    }
+
+    # ($a, $b, $c) = $image->Get('colorspace', 'magick', 'adjoin');
+    # $width = $image->[3]->Get('columns');
+    # user-time
+
+}
+
+
+=head2 ListFiles ( $path )
+
+Lists the files and folders in $path.
+
+Removes '.' and '..' from the list.
+
+=over
+
+=item $path
+
+The path to get a file list from.
+
+=item returns @list
+
+Returns a list of files and folders in $path.
+
+Returns an empty list on failure.
+
+=back
+
+=cut
+
+sub ListFiles
+{
+    my $path = shift;
+
+    if (!opendir(SOURCE_DIR, $path))
+    {
+        ClearConsoleLine();
+        print STDERR "Failed to open $path: $!\n";
+        return ();
+    }
+    my @list = readdir(SOURCE_DIR);
+    close(SOURCE_DIR);
+    
+    @list = File::Spec->no_upwards(@list);
+
+    return @list;
+}
+
+
+
+=head2 ClearConsoleLine ( $char )
+
+Prints spaces for the width of the current console line except for the very
+last character (in windows pringing the entire width of the line will
+automatically move the carrot one line down)
+
+Uses carrage return ("\r") in order to return to the start of the line.
+
+=over
+
+=item $char
+
+Optional.  Defaults to space (' ').
+
+If specified, this character will be used as the character to print the entire
+length of the line.
+
+=back
+
+=cut
+
+sub ClearConsoleLine
+{
+    my $char = shift // ' ';
+    my ($console_width_chars, undef, undef, undef) = GetTerminalSize();
+    print "\r";
+    for (my $x = 0; $x < $console_width_chars - 1; $x++) {print $char}
+    print "\r";
+}
+
+
+=head2 PadLeft ( $text, $length, $pad_with )
+
+Pads the left of the specified string to the specified length.
+
+=over
+
+=item $text
+
+the text to pad.
+
+default: an empty string
+
+=item $length
+
+the length to pad the text to.
+
+default: 0
+
+=item $pad_with
+
+the character to pad the string with.
+
+default: space character
+
+=item returns $padded
+
+the padded string.
+
+=back
+
+=cut
+
+sub PadLeft
+{
+    my $text = shift;
+    my $pad_len = shift;
+    my $pad_char = shift;
+    
+    if (!defined($text)) { $text = ""; }
+    if (!defined($pad_len)) { $pad_len = 0; }
+    if (!defined($pad_char)) { $pad_char = " "; }
+
+    my $padding = $pad_char x ( $pad_len - length( $text ) );
+    my $padded = $padding . $text;
+
+    return $padded;
+}
+
+
+=head2 PadRight ( $text, $length, $pad_with )
+
+Pads the right of the specified string to the specified length.
+
+=over
+
+=item $text
+
+the text to pad.
+
+default: an empty string
+
+=item $length
+
+the length to pad the text to.
+
+default: 0
+
+=item $pad_with
+
+the character to pad the string with.
+
+default: space character
+
+=item returns $padded
+
+the padded string.
+
+=back
+
+=cut
+
+sub PadRight
+{
+    my $text = shift;
+    my $pad_len = shift;
+    my $pad_char = shift;
+    
+    if (!defined($text)) { $text = ""; }
+    if (!defined($pad_len)) { $pad_len = 0; }
+    if (!defined($pad_char)) { $pad_char = " "; }
+
+    my $padding = $pad_char x ( $pad_len - length( $text ) );
+    my $padded = $text . $padding;
+
+    return $padded;
+}
+
+
+__END__
+
+=head1 AUTHOR
+
+Nathan Perry [nateperry333 at gmail dot com]
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright 2010 Nathan Perry.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
